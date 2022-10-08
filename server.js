6876a147c64f4f829e9a92d241e86db568222389
profeguille <guillermofergnani@gmail.com>
2022-10-08 13:43:14 -0300

    v2

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
-//console.log(process.argv);
+const path = require("path");
+const dotenv = require("dotenv");
+let modo = "test";
+require("dotenv").config({
+  path: path.resolve(__dirname, modo == "prod" ? ".env.prod" : ".env.testing"),
+});
+const { HOST, PORT } = require("./config");
+console.log(HOST, PORT);
 
-/* for (let k = 0; k < process.argv.length; k++) {
-  console.log(process.argv[k]);
-} */
+/* console.log(process.env.NOMBRE);
+console.log(process.env.PORT);
+console.log(process.env.SECRET_TWITTER);
+console.log(process.env.DATABASE_CONNECTION_STRING); */
+
+/* const yargs = require("yargs/yargs")(process.argv.slice(2));
+const args = yargs.alias({
+  n: "nombre",
+  a: "apellido",
+}).argv;
+
+console.log(args);
+console.log(process.argv.slice(2)); */
+
+/* const parseArgs = require("minimist");
+
+const args = parseArgs(process.argv.slice(2));
+
+console.log(args);
+console.log(process.argv.slice(2)); */
